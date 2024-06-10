e690bfbd75017fe6010dadcdcf190bbcd24fbadb
bleach4171 <167485971+bleach4171@users.noreply.github.com>
2024-06-10 15:36:36 +0200

    Add files via upload

---
+++ TaschenrechnerApp/TaschenrechnerApp/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaschenrechnerApp
+{
+    public partial class Form1 : Form
+    {
+        private decimal ersterWert = 0.0m; // der Erste Wert der eingegeben wird
+        private decimal zweiterWert = 0.0m; // zweite eingegebener Wert
+        private decimal Ergebnis = 0.0m; // Ergebnis der ersten Berechnung
+        private string operators = "+"; // Aktuell ausgewählter Operator
+
+        public TaschenrechnerApp()
+        {
+            InitializeComponent();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
